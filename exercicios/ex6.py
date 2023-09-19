19843ecb850265dbc9e63b644f4d2cb9abd75a67
Ryan  Vitor <ryanvitor29@gmail.com>
2023-09-19 00:51:14 -0300

    .

--- exercicios/ex6.py
+++ exercicios/ex6.py
@@ -1,7 +1,10 @@
+
 n1 = float(input('Digite a nota: '))
 n2 = float(input('Digite a nota: '))
 n3 = float(input('Digite a nota: '))
 n4 = float(input('Digite a nota: '))
 media = (n1 + n2 + n3 + n4)/4
 
-print(f'a média das notas é: {media}')
+print(f'a média das notas é: {media}') 
+
+
